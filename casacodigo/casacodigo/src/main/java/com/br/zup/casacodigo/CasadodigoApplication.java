eac3911743322e9c33756c005f2e9c4fcfd84e16
CaioNovoaAntunes <caio_novoa_antunes@mail.com>
2021-06-28 16:09:37 -0300

    subindo projeto

---
+++ casacodigo/casacodigo/src/main/java/com/br/zup/casacodigo/CasadodigoApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.casacodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasadodigoApplication.class, args);
+	}
+
+}
---
+++ casacodigo/casacodigo/src/test/java/com/br/zup/casacodigo/CasadodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.zup.casacodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
