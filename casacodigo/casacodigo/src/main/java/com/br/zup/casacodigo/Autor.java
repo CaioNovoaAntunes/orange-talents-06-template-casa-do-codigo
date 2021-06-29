dbd9469c2456a13db5cf526b5acc9675fdef46a9
CaioNovoaAntunes <caio_novoa_antunes@mail.com>
2021-06-29 15:50:41 -0300

    Classe Autor Criada

---
+++ casacodigo/casacodigo/src/main/java/com/br/zup/casacodigo/Autor.java
@@ -0,0 +1,46 @@
+package com.br.zup.casacodigo;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, length = 400)
+    private String descricao;
+    private LocalDateTime criadoEm;
+
+    @Deprecated
+    public Autor() {
+    }
+
+    public Autor(@NotBlank String nome, @NotBlank @Email String email, @NotBlank @Size(max = 400) String descricao) {
+        this.nome = nome;
+        this.email = email;
+        this.descricao = descricao;
+        this.criadoEm= LocalDateTime.now();
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
