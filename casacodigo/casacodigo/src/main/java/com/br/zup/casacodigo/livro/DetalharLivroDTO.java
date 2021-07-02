6c4fa93754422ad23517802d95c467858a7b3d8e
CaioNovoaAntunes <caio_novoa_antunes@mail.com>
2021-07-02 06:00:38 -0300

    livrodetails-logica-na-controller

---
+++ casacodigo/casacodigo/src/main/java/com/br/zup/casacodigo/livro/DetalharLivroDTO.java
@@ -0,0 +1,82 @@
+package com.br.zup.casacodigo.livro;
+
+
+import com.br.zup.casacodigo.autor.AutorResponse;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class DetalharLivroDTO {
+    private String titulo;
+    private String sumario;
+    private String resumo;
+    private BigDecimal preco;
+    private Integer numeroPaginas;
+    private String isbn;
+    private String categoria;
+    private AutorResponse autor;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataLancamento;
+
+    public DetalharLivroDTO(Livro livro) {
+        this.titulo= livro.getTitulo();
+        this.sumario= livro.getSumario();
+        this.resumo= livro.getResumo();
+        this.preco=livro.getPreco();
+        this.numeroPaginas= livro.getNumeroPaginas();
+        this.isbn= livro.getIsbn();
+        this.dataLancamento=livro.getDataLancamento();
+        this.categoria= livro.getCategoria().getNome();
+        this.autor=new AutorResponse(livro.getAutor());
+    }
+
+    public DetalharLivroDTO() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getSumario() {
+        return sumario;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public Integer getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public AutorResponse getAutor() {
+        return autor;
+    }
+
+    @Override
+    public String toString() {
+        return "DetalharLivroDTO{" +
+                "titulo='" + titulo + '\'' +
+                ", sumario='" + sumario + '\'' +
+                ", resumo='" + resumo + '\'' +
+                ", preco=" + preco +
+                ", numeroPaginas=" + numeroPaginas +
+                ", isbn='" + isbn + '\'' +
+                ", categoria=" + categoria +
+                ", autor=" + autor +
+                '}';
+    }
+}
--- casacodigo/casacodigo/src/main/java/com/br/zup/casacodigo/livro/LivroController.java
+++ casacodigo/casacodigo/src/main/java/com/br/zup/casacodigo/livro/LivroController.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -25,6 +26,15 @@ public class LivroController {
     public ResponseEntity<?> listarLivros() {
         List<Livro> livros = livroRepository.findAll();
         return ResponseEntity.ok(livros.stream().map(LivroResponse::new).collect(Collectors.toList()));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<DetalharLivroDTO> listarLivros(@PathVariable Long id) {
+        Optional<Livro> livro = livroRepository.findById(id);
+        if(!livro.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        DetalharLivroDTO dto = new DetalharLivroDTO(livro.get());
+        return ResponseEntity.ok(dto);
     }
 
 
